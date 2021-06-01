c2fd3912dce1dc4c497e67237c166fb8134c81fd
rose129 <mkos__@nate.com>
2021-06-01 20:48:34 +0900

    fullpage 기본셋팅

---
+++ common/js/jihye2.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#myfull').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+    sectionsColor : ['red', 'coral' ,'pink','yellow'],
+    navigation: true,
+    navigationPosition: 'left',
+	  navigationTooltips: ['portfolio', 'aboutme','social','contect'],
+    slidesNavigation: true,
+	});
+
+
+
+});
